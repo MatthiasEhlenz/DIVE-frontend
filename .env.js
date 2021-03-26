c14bd8d46ce5cb62773da226a8deb9e884ce561b
aneesha <aneesha.bakharia@gmail.com>
2021-03-26 22:32:26 +1000

    Update .env.js

--- .env.js
+++ .env.js
@@ -1,5 +1,7 @@
+//API_URL: process.env.API_URL || 'http://localhost:8081',
+
 module.exports = {
-  API_URL: process.env.API_URL || 'http://localhost:8081',
+  API_URL: process.env.API_URL || 'http://localhost/api',
   NODE_ENV: process.env.NODE_ENV || 'DEVELOPMENT',
   SUBDOMAIN: process.env.SUBDOMAIN || 'staging'
 };
