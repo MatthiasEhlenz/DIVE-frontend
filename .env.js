7669cdc68efee3e56ad0e2d228117fadb0015d05
aneesha <aneesha.bakharia@gmail.com>
2021-04-08 22:06:54 +1000

    Update .env.js

--- .env.js
+++ .env.js
@@ -1,7 +1,7 @@
 //API_URL: process.env.API_URL || 'http://localhost:8081',
 
 module.exports = {
-  API_URL: process.env.API_URL || 'http://localhost/api',
+  API_URL: process.env.API_URL || 'https://www.la-dive.com/api',
   NODE_ENV: process.env.NODE_ENV || 'DEVELOPMENT',
   SUBDOMAIN: process.env.SUBDOMAIN || 'staging'
 };
